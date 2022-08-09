50015ec03c899f5300b3835adf203a66daf6ae78
Arsen Batyuchok <arsen.batyuchok@gmail.com>
2022-08-09 12:26:47 +0300

    re-export of all modules from core & sdk, LayoutStyle component

---
+++ src/components/LayoutStyle.tsx
@@ -0,0 +1,22 @@
+import React, { FC } from 'react';
+import { getLayoutMediaQuery, TLayout } from '@cntrl-site/core';
+// @ts-ignore
+import JSXStyle from 'styled-jsx/style';
+
+export interface LayoutStyleProps {
+  id: string;
+  layouts: TLayout[];
+  layoutId: string;
+  children?: (layout: TLayout) => string;
+}
+
+export const LayoutStyle: FC<LayoutStyleProps> = ({ id, layouts, layoutId, children }) => {
+  const layout = layouts.find(l => l.id === layoutId)!;
+  return (
+    <JSXStyle id={id}>{`
+      ${getLayoutMediaQuery(layoutId, layouts)} {
+        ${children?.(layout)}
+      }
+    `}</JSXStyle>
+  );
+};
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
-export { RichTextConverter } from './utils/RichTextConverter';
+export * from '@cntrl-site/core';
+export * from '@cntrl-site/sdk';
 
+export { RichTextConverter } from './utils/RichTextConverter';
 export { Page } from './components/Page';
 export { Article } from './components/Article';
 export { Section } from './components/Section';
@@ -8,3 +10,4 @@ export { ImageItem } from './components/items/ImageItem';
 export { RectangleItem } from './components/items/RectangleItem';
 export { RichTextItem } from './components/items/RichTextItem';
 export { VideoItem } from './components/items/VideoItem';
+export { LayoutStyle } from './components/LayoutStyle';
