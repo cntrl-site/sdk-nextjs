7bb02fab0de0fee8cc331e2d4928c2ebf536da7c
Paula <paulet1207@gmail.com>
2022-10-24 13:15:56 +0300

    removed fullwidth + sdk version bump

---
+++ src/components/items/VimeoEmbed.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+import { ItemProps } from '../Item';
+import { VimeoEmbedItem } from '@cntrl-site/core/src/article/ArticleItems';
+
+export const VimeoEmbed:FC<ItemProps<VimeoEmbedItem>> = () => (
+  // TODO
+  <>
+  </>
+)
