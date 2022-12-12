196339d67e7ce8b665dbbcd52d9ac4ee2ea8526b
pylypchukroman <romanpylypchuk@gmail.com>
2022-12-12 18:34:43 +0200

    CNTRL-694 added code styling and version bump

--- src/components/LinkWrapper.tsx
+++ src/components/LinkWrapper.tsx
@@ -9,7 +9,6 @@ export const LinkWrapper: React.FC<Props> = ({ url, children }) => {
   const validUrl = validUrlBuild(url!);
   return url ? (
     <a
-      // href={!url.startsWith('http://') && !url.startsWith('https://') && !url.startsWith('/') ? `//${url}`: url}
       href={validUrl}
       target={url.startsWith('/') ? '_self' : '_blank'}
       rel="noreferrer"
@@ -23,7 +22,17 @@ export const LinkWrapper: React.FC<Props> = ({ url, children }) => {
 
 
 function validUrlBuild(url: string): string {
-  const protocols = ['http://', 'https://', '/', 'mailto:', 'tel:', 'file:', 'ftp:', 'javascript', '#'];
+  const protocols = [
+    'http://',
+    'https://',
+    '/',
+    'mailto:',
+    'tel:',
+    'file:',
+    'ftp:',
+    'javascript',
+    '#'
+  ];
   const protocolCheck = protocols.some(protocol => url.startsWith(protocol));
   if (protocolCheck) return url;
   return `//${url}`;
