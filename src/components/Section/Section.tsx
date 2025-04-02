64afd334e411ba11711f7553169a806eaba47c1d
Paula <paulet1207@gmail.com>
2025-04-02 16:47:18 +0200

    CNTRL-2085: updated section background to sticky 2

--- src/components/Section/Section.tsx
+++ src/components/Section/Section.tsx
@@ -35,7 +35,6 @@ export const Section: FC<Props> = ({ section, data, children }) => {
   const SectionComponent = section.name ? customSections.getComponent(section.name) : undefined;
   const isVideo = layout && section.background?.[layout]?.url ? isVideoUrl(String(section.background?.[layout]?.url)) : false;
   const videoRef = useRef<HTMLVideoElement | null>(null);
-  const sectionRectRef = useRef<DOMRect | null>(null);
   useSectionRegistry(section.id, sectionRef.current);
 
   const getSectionVisibilityStyles = () => {
@@ -58,16 +57,14 @@ export const Section: FC<Props> = ({ section, data, children }) => {
     const video = videoRef.current;
     const section = sectionRef.current;
     const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting) {
-            video.style.display = 'block';
-            video.play().catch(() => {});
-          } else {
-            video.style.display = 'none';
-            video.pause();
-          }
-        });
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          video.style.display = 'block';
+          video.play().catch(() => {});
+        } else {
+          video.style.display = 'none';
+          video.pause();
+        }
       },
       {
         root: null,
@@ -79,36 +76,14 @@ export const Section: FC<Props> = ({ section, data, children }) => {
     return () => observer.disconnect();
   }, [isVideo]);
 
-  useEffect(() => {
-    if ((layout && sectionRef.current && section.background?.[layout]?.position !== 'fixed') || !isVideo) return;
-    let rafId: number;
-    const updateMask = (): void => {
-      sectionRectRef.current = sectionRef.current!.getBoundingClientRect();
-      const video = videoRef.current;
-      const rect = sectionRectRef.current;
-      if (!rect || !video) return;
-      const maskTop = (rect.top).toFixed(2);
-      const maskBottom = (window.innerHeight - (rect.height + rect.top)).toFixed(2);
-      video.style.clipPath = `inset(${maskTop}px 0 ${maskBottom}px 0)`;
-      rafId = requestAnimationFrame(updateMask);
-    };
-    rafId = requestAnimationFrame(updateMask);
-    return () => {
-      cancelAnimationFrame(rafId);
-    };
-  }, [layout, section.id, isVideo]);
-
-  useEffect(() => {
-    if ((layout && sectionRef.current && section.background?.[layout]?.position !== 'fixed') || !isVideo) return;
-    const handleResize = () => {
-      sectionRectRef.current = sectionRef.current!.getBoundingClientRect();
-    };
-    handleResize();
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-  }, [layout, section.id, isVideo]);
-
   if (SectionComponent) return <div ref={sectionRef}><SectionComponent data={data}>{children}</SectionComponent></div>;
+
+  // const bg = layout && section.background ? section.background?.[layout] : null;
+  // const style = bg ? {
+  //   backgroundSize: bg.size === 'auto'
+  //     ? `${bg.percentage}%`
+  //     : bg.size
+  // } : {};
 
   return (
     <>
@@ -118,21 +93,23 @@ export const Section: FC<Props> = ({ section, data, children }) => {
         ref={sectionRef}
       >
         {isVideo && sectionRef.current && layout && (
-          <div
-            key={`video-background-wrapper-${section.id}`}
-            className={`video-background-wrapper-${section.id}`}
-          >
-            <video
-              ref={videoRef}
-              autoPlay
-              loop
-              muted
-              playsInline
-              preload="auto"
-              className={`video-background-${section.id}`}
+          <div className={`video-background-overlay-${section.id}`}>
+            <div
+              key={`video-background-wrapper-${section.id}`}
+              className={`video-background-wrapper-${section.id}`}
             >
-              <source src={section.background?.[layout]?.url ?? ''} />
-            </video>
+              <video
+                ref={videoRef}
+                autoPlay
+                loop
+                muted
+                playsInline
+                preload="auto"
+                className={`video-background-${section.id}`}
+              >
+                <source src={section.background?.[layout]?.url ?? ''} />
+              </video>
+            </div>
           </div>
         )}
         {children}
@@ -145,28 +122,31 @@ export const Section: FC<Props> = ({ section, data, children }) => {
             position: relative;
             background-color: ${CntrlColor.parse(color ?? DEFAULT_COLOR).fmt('rgba')};
             background-image: ${background && !isVideo && background.size !== 'none' ? `url(${background.url})` : 'none'};
-            background-size: ${background?.size === 'auto' ? `${background?.percentage ?? '50'}%` : background?.size ?? ''};
+            background-size: ${background?.size === 'auto' ? `${background?.percentage ?? '50'}%` : background?.size};
             background-position: center;
             background-repeat: ${background?.size === 'contain' ? 'no-repeat' : 'repeat'};
             background-attachment: ${background?.position ?? ''};
          }
-         .video-background-wrapper-${section.id} {
-            top: 0;
-            left: 0;
-            position: ${background?.position === 'fixed' ? 'fixed' : 'relative'};
-            height: 100%;
+         .video-background-overlay-${section.id} {
+            height: ${getSectionHeight(height)};
             width: 100%;
-            overflow: hidden;
+            position: relative;
+            overflow: clip;
+         }
+         .video-background-wrapper-${section.id} {
+            transform: translateY(-100vh);
+            left: 0;
+            position: relative;
+            height: calc(${getSectionHeight(height)} + 200vh);
+            width: 100%;
          }
          .video-background-${section.id} {
             object-fit: ${background?.size ?? 'cover'};
             width: 100%;
-            height: 100%;
-            position: absolute;
-            top: 0;
+            height: ${background?.position === 'fixed' ? '100vh' : '100%'};
+            position: ${background?.position === 'fixed' ? 'sticky' : 'relative'};
+            top: ${background?.position === 'fixed' ? '100vh' : 'unset'};
             left: 0;
-            will-change: transform;
-            transform: translateZ(0);
          }
         `
     ))
